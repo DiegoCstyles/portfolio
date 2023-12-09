5645be783429664b554d8d48d553ae2de21b8f74
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-09 19:02:39 -0300

    Update NavbarPage.tsx

--- components/Navbar/NavbarPage.tsx
+++ components/Navbar/NavbarPage.tsx
@@ -33,7 +33,7 @@ const NavbarPage = ({ page, currentLanguage }: NavbarProps) => {
             <Link className="block text-sm py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 hover:underline hover:decoration-lime-600 dark:text-white dark:hover:decoration-blendedCyan hover:underline-offset-8 overline transition delay-100 duration-200 ease-in zoom-effect" 
                 aria-current="page" 
                 href={"/"+ page}>
-                {translations[currentLanguage][page]}
+                {(translations[currentLanguage] as any)[page]}
             </Link>
         </li>
       )
