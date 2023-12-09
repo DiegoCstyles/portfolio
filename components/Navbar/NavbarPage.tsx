2b7a63a6b8811e5aac99c660d5b9ff627162685d
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-09 18:56:04 -0300

    Update NavbarPage.tsx

--- components/Navbar/NavbarPage.tsx
+++ components/Navbar/NavbarPage.tsx
@@ -5,10 +5,23 @@ interface NavbarProps {
     page: string;
 }
 
-const NavbarPage = ({ page }: NavbarProps) => {
+const NavbarPage = ({ page }: NavbarProps, currentLanguage) => {
+    const translations = {
+    en: {
+      about: 'About',
+      projects: 'Projects',
+      contact: 'Contact',
+    },
+    'pt-br': {
+      about: 'Sobre',
+      projects: 'Projetos',
+      contact: 'Contato',
+    },
+  };
+    
     return (
         <li>
-            <Link className="block text-sm py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 hover:underline hover:decoration-lime-600 dark:text-white dark:hover:decoration-blendedCyan hover:underline-offset-8 overline transition delay-100 duration-200 ease-in zoom-effect" aria-current="page" href={"/"+ page}>{page.toUpperCase()}</Link>
+            <Link className="block text-sm py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 hover:underline hover:decoration-lime-600 dark:text-white dark:hover:decoration-blendedCyan hover:underline-offset-8 overline transition delay-100 duration-200 ease-in zoom-effect" aria-current="page" href={"/"+ page}>{translations[currentLanguage][page]}</Link>
         </li>
       )
 }
