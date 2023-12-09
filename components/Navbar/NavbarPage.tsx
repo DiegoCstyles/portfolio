8a0718c37dc23a00fe0bed3cee60d2cfd45a03ea
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-09 18:59:44 -0300

    Update NavbarPage.tsx

--- components/Navbar/NavbarPage.tsx
+++ components/Navbar/NavbarPage.tsx
@@ -3,9 +3,10 @@ import '../../app/styles/fadeIn.css';
 
 interface NavbarProps {
     page: string;
+    currentLanguage: string;
 }
 
-const NavbarPage = ({ page }: NavbarProps, currentLanguage) => {
+const NavbarPage = ({ page, currentLanguage }: NavbarProps) => {
     const translations = {
     en: {
       about: 'About',
@@ -21,7 +22,11 @@ const NavbarPage = ({ page }: NavbarProps, currentLanguage) => {
     
     return (
         <li>
-            <Link className="block text-sm py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 hover:underline hover:decoration-lime-600 dark:text-white dark:hover:decoration-blendedCyan hover:underline-offset-8 overline transition delay-100 duration-200 ease-in zoom-effect" aria-current="page" href={"/"+ page}>{translations[currentLanguage][page]}</Link>
+            <Link className="block text-sm py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 hover:underline hover:decoration-lime-600 dark:text-white dark:hover:decoration-blendedCyan hover:underline-offset-8 overline transition delay-100 duration-200 ease-in zoom-effect" 
+                aria-current="page" 
+                href={"/"+ page}>
+                {translations[currentLanguage][page]}
+            </Link>
         </li>
       )
 }
