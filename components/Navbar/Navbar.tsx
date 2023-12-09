9e78c620e10b6ae65617899f2b4e88593fe4b1e8
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-09 19:28:02 -0300

    Update Navbar.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -7,9 +7,10 @@ import { useMediaQuery } from 'react-responsive';
 
 interface NavbarProps {
   currentLanguage: string;
+  onLanguageChange: () => void;
 }
 
-const Navbar = ({ currentLanguage }: NavbarProps) => {
+const Navbar = ({ currentLanguage, onLanguageChange }: NavbarProps) => {
   const isSmallScreen = useMediaQuery({ maxWidth: 767 }); // Set the breakpoint for small screens
   
     return (
@@ -35,7 +36,7 @@ const Navbar = ({ currentLanguage }: NavbarProps) => {
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
             <li
               className={`cursor-pointer text-sm text-white dark:text-white`}
-              onClick={handleLanguageChange}
+              onClick={onLanguageChange}
             >
               {currentLanguage.toUpperCase()}
             </li>
