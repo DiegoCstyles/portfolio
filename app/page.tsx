33fda910dc83820cfbff9f518024a222a6f4dc4c
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-15 20:01:53 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from 'react';
 import './styles/fadeIn.css';
+import './styles/home.css';
 
 // Check if 'window' is defined before importing components that may use it
 const isBrowser = typeof window !== 'undefined';
@@ -39,12 +40,7 @@ export default function Home() {
   }, []); // Empty dependency array ensures the effect runs only once on mount
 
   return (
-      <main style={{
-        backgroundImage: 'linear-gradient(45deg, #000000, #000000, #9CBBE2 )',
-        backgroundSize: '200% 200%',
-        animation: 'gradientAnimation 15s ease infinite',
-        transform: 'translateY(var(--section-offset))'
-      }} className="overflow-hidden" onMouseMove={handleMouseMove}>
+      <main className="main-container overflow-hidden" onMouseMove={handleMouseMove}>
         {/* Circular Cursor */}
         <div className="circular-cursor absolute w-12 h-12 rounded-full bg-white bg-opacity-70 pointer-events-none"
             style={{ left: cursorPosition.x-22, top: cursorPosition.y-22 }}>
