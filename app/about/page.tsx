573b23aeaa425c382235257e4af294e87a5ad221
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-15 22:06:16 -0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -42,7 +42,7 @@ export default function About() {
   }, []); // Empty dependency array ensures the effect runs only once on mount
 
   return (
-    <div className="main-container light:ltBackground dark:dkBackground overflow-hidden" onMouseMove={handleMouseMove}>
+    <div className="light:ltBackground dark:dkBackground overflow-hidden" onMouseMove={handleMouseMove}>
       {/* Circular Cursor */}
       <div className="circular-cursor absolute w-12 h-12 rounded-full bg-white bg-opacity-70 pointer-events-none"
             style={{ left: cursorPosition.x-22, top: cursorPosition.y-22 }}>
