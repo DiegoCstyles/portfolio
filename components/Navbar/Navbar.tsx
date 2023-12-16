7eaf2ac325d897d506e5df975fe9dca110bf6f7a
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-15 21:16:32 -0300

    Update Navbar.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = ({ currentLanguage, onLanguageChange }: NavbarProps) => {
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
             <li
-              className={`cursor-pointer text-sm text-white dark:text-white`}
+              className={`cursor-pointer text-sm text-black dark:text-white`}
               onClick={onLanguageChange}
             >
               {currentLanguage.toUpperCase()}
