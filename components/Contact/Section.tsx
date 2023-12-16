ecfc524f309d589f733b56bbac4147697be2ea44
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-16 09:26:05 -0300

    Update Section.tsx

--- components/Contact/Section.tsx
+++ components/Contact/Section.tsx
@@ -136,7 +136,7 @@ const Section = () => {
       </div>
       <div className="zoom-effect mx-auto ">
         <div className=" w-full h-full relative rounded-md overflow-hidden">
-            <Image style={{ transform: 'translateY(var(--image-offset))' }} src={ContactImage} alt="me" className="object-cover inset-0 bg-transparent opacity-40" width={400} height={300} />
+            <Image style={{ transform: 'translateY(var(--image-offset))' }} src={ContactImage} alt="me" className="object-cover inset-0 bg-transparent opacity-70 dark:opacity-40" width={400} height={300} />
         </div>
       </div>
     </section>
