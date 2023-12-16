fd8ae4e0d63f7455339d030fdac5ac2e360479df
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-16 17:42:01 -0300

    Update Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,11 +3,31 @@ import FooterActions from "./FooterActions";
 import React, { memo } from 'react';
 import '../../app/styles/fadeIn.css';
 
-const Footer = () => {
+interface FooterProps {
+  currentLanguage: string;
+}
+
+interface FooterTranslations {
+  title: string;
+  description: string;
+}
+
+const translations: { [language: string]: FooterTranslations } = {
+  en: {
+      h1: 'crafting delightful web experiences',
+    },
+    'pt-br': {
+      h1: 'criando experiências web magníficas',
+    },
+};
+
+
+const Footer = ({ currentLanguage }: FooterProps) => {
+  
   return (
     <div className="border bg-black/50 rounded-t-md border-black dark:border-white max-w-screen-xl flex flex-wrap items-center mx-auto p-2 sm:p-2 md:p-4 lg:p-5 justify-center ">
       <div className="typewriter text-black dark:text-white text-sm mr-4">
-        <h1>crafting delightful web experiences</h1>
+        <h1>{translations[currentLanguage].h1}</h1>
       </div>
       <span className="flex-grow"></span>
       
