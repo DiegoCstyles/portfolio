987b5b24fa9156226299aca3597f12be3d1f2c37
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-15 21:40:54 -0300

    Update NavbarTitle.tsx

--- components/Navbar/NavbarTitle.tsx
+++ components/Navbar/NavbarTitle.tsx
@@ -6,7 +6,7 @@ const NavbarTitle = () => {
         <Link href="/" className="flex items-center group">
                 <span className="dark:text-white zoom-effect self-center top-0 left-0 text-2xl font-semibold whitespace-nowrap ">
 
-                    <div className='flex'><span>Diego Costa</span> <span className='ml-1 text-blendedCyan'>/&gt;</span></div> 
+                    <div className='flex'><span>Diego Costa</span> <span className='ml-1 text-green-500 dark:text-blendedCyan'>/&gt;</span></div> 
 
                 </span>
         </Link>
