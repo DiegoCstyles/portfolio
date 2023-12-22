b6c45869bb257b11d9e060f6b7d0b9b0b2e49aff
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-22 12:03:58 -0300

    Update Icon.tsx

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -26,7 +26,7 @@ const Icon = ({
   const { theme } = useTheme();
   const darkThemeColor = 'rgb(0, 0, 0)';
   const lightThemeColor = 'rgb(255, 255, 255)';
-  const color;
+  let color;
 
   // Validate theme and set color accordingly
   if (theme === 'dark') {
@@ -41,7 +41,7 @@ const Icon = ({
     <Link href={href} target={target} rel="noopener noreferrer" aria-label={label} className={classNameLink}>
       <div className="flex items-center " onClick={onClick}>
         {children}
-        <Icon className={" w-6 h-6 md:w-5 md:h-5 lg:w-5 lg:h-5 opacity-80 group-hover:opacity-100 transition duration-300 " + classNameIcon} color={color} />
+        <Icon className={" w-6 h-6 md:w-5 md:h-5 lg:w-5 lg:h-5 opacity-80 group-hover:opacity-100 transition duration-300 " + classNameIcon} style={{ color }} />
       </div>
     </Link>
   );
