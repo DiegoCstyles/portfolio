c997f5ca07555833f8bca7fbd07ff2e4e04e3257
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-23 09:37:04 -0300

    Update NavbarTitle.tsx

--- components/Navbar/NavbarTitle.tsx
+++ components/Navbar/NavbarTitle.tsx
@@ -4,9 +4,9 @@ import '../../app/styles/fadeIn.css';
 const NavbarTitle = () => {
     return (
         <Link href="/" className="flex items-center group">
-                <span className="dark:text-white zoom-effect self-center top-0 left-0 text-lg font-medium whitespace-nowrap ">
+                <span className="dark:text-white zoom-effect self-center top-0 left-0 text-xl font-medium whitespace-nowrap ">
 
-                    <div className='flex'><span>Diego Costa</span> <span className='ml-1 text-fuchsia-600 dark:text-fuchsia-600'>/&gt;</span></div> 
+                    <div className='flex'><span>Diego Costa</span> <span className='ml-1 text-fuchsia-500 dark:text-fuchsia-500'>/&gt;</span></div> 
 
                 </span>
         </Link>
