b1fa22d81462538f61a964ffa0f5c43053b085fb
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-23 15:11:50 -0300

    Update Section.tsx

--- components/Projects/Section.tsx
+++ components/Projects/Section.tsx
@@ -102,7 +102,7 @@ const Section = ({ currentLanguage }: ProjectsProps) => {
               
               <div key={index} className="mt-3 text-sm justify-center items-center ">
                 <ol className="relative border-l border-black dark:border-gray-700">
-                  <span className="absolute flex items-center justify-center w-6 h-6 bg-green-700 dark:bg-lime-900 rounded-full -left-3 ring-8 ring-white/5 dark:ring-gray-900/5">
+                  <span className="absolute flex items-center justify-center w-6 h-6 bg-white dark:bg-white rounded-full -left-3 ring-8 ring-white/5 dark:ring-gray-900/5">
                       <svg className="w-2.5 h-2.5 text-fuchsia-500 dark:text-fuchsia-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                           <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                       </svg>
