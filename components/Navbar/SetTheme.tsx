06410a8c2b53ed9708691936b06df135a798944a
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-23 10:01:09 -0300

    Update SetTheme.tsx

--- components/Navbar/SetTheme.tsx
+++ components/Navbar/SetTheme.tsx
@@ -6,7 +6,7 @@ const SetTheme = () => {
     const { theme, setTheme } = useTheme()
     return (
         <li>
-            <span className="dark:text-white cursor-pointer zoom-effect" onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+            <span className="cursor-pointer block text-sm p-3 dark:text-white transition delay-100 duration-200 ease-in zoom-effect hover:bg-fuchsia-600/50" onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
                 {theme === 'light' ? (
                     <CgDarkMode className=" inline-block w-7 h-5 mr-1 hover:text-green-500 dark:hover:text-lime-600 transition delay-100 duration-200 ease-in" />
                   ) : (
