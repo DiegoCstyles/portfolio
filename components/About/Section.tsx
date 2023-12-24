82246c4f995c8cea3ee52d00e8be26799dbceb2c
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-24 20:31:34 -0300

    Update Section.tsx

--- components/About/Section.tsx
+++ components/About/Section.tsx
@@ -150,7 +150,7 @@ const Section = ({ currentLanguage }: AboutProps) => {
   return (
       <section className=" hero flex-auto py-24 flex items-center justify-center animate-fade-in bg-fuchsia-950">
         
-        <div className="mx-auto max-w-[55rem] text-center">
+        <div className="mx-auto max-w-[55rem] text-center bg-white/30 ">
             {/* Heading */}
             <div className="flex flex-auto">
               <h1 className=" text-white dark:text-white glitch-effect1 flex-1 mt-3 text-7xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-7xl leading-[4rem] tracking-tight">
@@ -161,7 +161,7 @@ const Section = ({ currentLanguage }: AboutProps) => {
             </div>
 
             {/* about */}
-            <div className="text-sm tracking-tight font-medium leading-relaxed text-black dark:text-black bg-white/30 rounded-md">
+            <div className="text-sm tracking-tight font-medium leading-relaxed text-black dark:text-black">
               <p className="mt-10">
                 {translations[currentLanguage].journeyP1}
                 <strong className="text-black dark:text-white uppercase">
