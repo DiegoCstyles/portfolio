5348a088d1a06fda87d40d8ddccb507f6959a246
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-26 17:53:26 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from 'react';
 import './styles/home.css';
+import { Link as ScrollLink, animateScroll } from 'react-scroll';
 
 // Check if 'window' is defined before importing components that may use it
 const isBrowser = typeof window !== 'undefined';
@@ -12,6 +13,7 @@ const ContactSection = isBrowser ? require('@/components').ContactSection : null
 const Footer = isBrowser ? require('@/components').Footer : null;
 
 export default function Home() {
+  const router = useRouter();
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
   const [currentLanguage, setCurrentLanguage] = useState('en');
 
@@ -41,14 +43,34 @@ export default function Home() {
     }
   }, []); // Empty dependency array ensures the effect runs only once on mount
 
+  const scrollTo = (section: string) => {
+    animateScroll.scrollToTop(); // Scroll to top to avoid conflicts with existing scroll position
+    const targetElement = document.getElementById(section);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
       <main className="light:ltBackground dark:dkBackground overflow-hidden" onMouseMove={handleMouseMove}>
         {/* Sections */}
         {Navbar && <Navbar currentLanguage={currentLanguage} onLanguageChange={handleLanguageChange}/>}
         {Hero && <Hero currentLanguage={currentLanguage}/>}
-        {AboutSection && <AboutSection currentLanguage={currentLanguage}/>}
-        {ProjectsSection && <ProjectsSection currentLanguage={currentLanguage}/>}
-        {ContactSection && <ContactSection currentLanguage={currentLanguage}/>}
+        {AboutSection && (
+          <section id="about">
+            <AboutSection currentLanguage={currentLanguage} />
+          </section>
+        )}
+        {ProjectsSection && (
+          <section id="projects">
+            <ProjectsSection currentLanguage={currentLanguage} />
+          </section>
+        )}
+        {ContactSection && (
+          <section id="contact">
+            <ContactSection currentLanguage={currentLanguage} />
+          </section>
+        )}
         {Footer && <Footer currentLanguage={currentLanguage}/>}
       </main>
   )
