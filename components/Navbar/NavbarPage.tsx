6739e0c0bd04d7a233b7d7d8c6fa0156f6c462a5
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-26 18:23:20 -0300

    Update NavbarPage.tsx

--- components/Navbar/NavbarPage.tsx
+++ components/Navbar/NavbarPage.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import '../../app/styles/fadeIn.css';
+import { Link as ScrollLink, animateScroll } from "react-scroll";
 
 interface NavbarProps {
     page: string;
@@ -16,25 +17,41 @@ interface Translations {
 
 const NavbarPage = ({ page, currentLanguage }: NavbarProps) => {
     const translations: Translations = {
-    en: {
-      about: 'About',
-      projects: 'Projects',
-      contact: 'Contact',
-    },
-    'pt-br': {
-      about: 'Sobre',
-      projects: 'Projetos',
-      contact: 'Contato',
-    },
-  };
+        en: {
+          about: 'About',
+          projects: 'Projects',
+          contact: 'Contact',
+        },
+        'pt-br': {
+          about: 'Sobre',
+          projects: 'Projetos',
+          contact: 'Contato',
+        },
+      };
+
+     const scrollTo = (section: string) => {
+        animateScroll.scrollToTop(); // Scroll to top to avoid conflicts with existing scroll position
+        const targetElement = document.getElementById(section);
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: "smooth" });
+        }
+      };
     
     return (
         <li>
-            <Link className="block text-sm p-3 dark:text-white transition delay-100 duration-200 ease-in zoom-effect hover:bg-fuchsia-600/50" 
-                aria-current="page" 
-                href={"#"}>
-                {(translations[currentLanguage] as any)[page].toUpperCase()}
-            </Link>
+            <Link href="#">
+                {/* Wrap Link content in ScrollLink for smooth scrolling */}
+                <ScrollLink
+                  className="block text-sm p-3 dark:text-white transition delay-100 duration-200 ease-in zoom-effect hover:bg-fuchsia-600/50"
+                  to={page.toLowerCase()} // Assumes that your sections have the same IDs as page names
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  offset={-70} // Adjust the offset as needed
+                >
+                  {(translations[currentLanguage] as any)[page].toUpperCase()}
+                </ScrollLink>
+              </Link>
         </li>
       )
 }
