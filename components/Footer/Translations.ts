a7a95ffbe7470e71855ec76b1b96a793772aad89
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-27 20:38:03 -0300

    Create Translations.ts

---
+++ components/Footer/Translations.ts
@@ -0,0 +1,14 @@
+interface FooterTranslations {
+  h1: string;
+}
+
+const translations: { [language: string]: FooterTranslations } = {
+  en: {
+      h1: 'crafting delightful web experiences',
+    },
+    'pt-br': {
+      h1: 'criando experiências web magníficas',
+    },
+};
+
+export default translations;
