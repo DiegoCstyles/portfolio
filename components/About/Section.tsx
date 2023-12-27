c4f285bb4445de9c8cf4894e6362b7fb946e546e
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-27 17:55:45 -0300

    Update Section.tsx

--- components/About/Section.tsx
+++ components/About/Section.tsx
@@ -38,10 +38,10 @@ const Section = ({ currentLanguage }: AboutProps) => {
         <div className="mx-auto max-w-[55rem] text-center">
             {/* Heading */}
             <div className="flex flex-auto">
-              <h1 className=" text-white dark:text-white glitch-effect1 flex-1 mt-3 text-7xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-7xl leading-[4rem] tracking-tight">
+              <h1 className=" text-black dark:text-black glitch-effect1 flex-1 mt-3 text-7xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-7xl leading-[4rem] tracking-tight">
                 {translations[currentLanguage].h1}
-                <span className="glitch-effect2 text-black dark:text-black" data-text={translations[currentLanguage].h1}></span>
-                <span className="glitch-effect3 text-white dark:text-white" data-text={translations[currentLanguage].h1}></span>
+                <span className="glitch-effect2 text-white dark:text-white" data-text={translations[currentLanguage].h1}></span>
+                <span className="glitch-effect3 text-black dark:text-black" data-text={translations[currentLanguage].h1}></span>
               </h1>
             </div>
 
@@ -49,21 +49,21 @@ const Section = ({ currentLanguage }: AboutProps) => {
             <div className="text-sm tracking-tight font-medium leading-relaxed text-white dark:text-white">
               <p className="mt-10">
                 {translations[currentLanguage].journeyP1}
-                <strong className="text-fuchsia-600 uppercase">
+                <strong className="light:text-black dark:text-fuchsia-600 uppercase">
                   {translations[currentLanguage].journeyP2}
                 </strong>
                 {translations[currentLanguage].journeyP3}
               </p>
               <p className="mt-3">
                 {translations[currentLanguage].careerP1}
-                <strong className="text-fuchsia-600 uppercase">
+                <strong className="light:text-black dark:text-fuchsia-600 uppercase">
                   {translations[currentLanguage].careerP2}
                 </strong>
                 {translations[currentLanguage].careerP3}
               </p>
               <p className="mt-3">
                 {translations[currentLanguage].freeTimeP1}
-                <strong className="text-fuchsia-600 uppercase">
+                <strong className="light:text-black dark:text-fuchsia-600 uppercase">
                   {translations[currentLanguage].freeTimeP2}
                 </strong>
                 {translations[currentLanguage].freeTimeP3}
@@ -81,7 +81,7 @@ const Section = ({ currentLanguage }: AboutProps) => {
               <div onMouseEnter={() => handleSpanHover('firstSpan')} onMouseLeave={() => handleSpanHover(null)} className={`zoom-effect flex items-start mt-3 text-base leading-relaxed text-black dark:text-black p-1.5 ${hoveredSpan === 'firstSpan' ? 'active' : ''}`}>
                 <div className='text-left text-white'>
                   <div className='flex'>
-                    <p className=" border border-black dark:border-white rounded-md text-center w-96 "> • {translations[currentLanguage].firstCareerTitle}</p>
+                    <p className=" border border-black dark:border-white rounded-md text-center light:text-black dark:text-white w-96"> • {translations[currentLanguage].firstCareerTitle}</p>
                   </div>
                   <p className="mt-1 text-xs font-medium text-center">AtendeiTi</p>
                   <p className="mt-1 text-xs font-medium text-center">2022 - 2023</p>
@@ -96,7 +96,7 @@ const Section = ({ currentLanguage }: AboutProps) => {
               <div onMouseEnter={() => handleSpanHover('secondSpan')} onMouseLeave={() => handleSpanHover(null)} className={`zoom-effect flex items-start mt-3 text-base leading-relaxed text-black dark:text-black p-1.5 ${hoveredSpan === 'secondSpan' ? 'active' : ''}`}>
                 <div className='text-left text-white'>
                   <div className='flex'>
-                    <p className="border border-black dark:border-white rounded-md text-center w-96"> • {translations[currentLanguage].secondCareerTitle}</p>
+                    <p className="border border-black dark:border-white rounded-md text-center light:text-black dark:text-white w-96"> • {translations[currentLanguage].secondCareerTitle}</p>
                     
                   </div>
                     <p className="mt-1 text-xs font-medium text-center">Objetivo Lorena</p>
@@ -123,7 +123,7 @@ const Section = ({ currentLanguage }: AboutProps) => {
               <div onMouseEnter={() => handleSpanHover('thirdSpan')} onMouseLeave={() => handleSpanHover(null)} className={`zoom-effect flex items-start mt-3 text-base leading-relaxed text-black dark:text-black p-1.5 ${hoveredSpan === 'thirdSpan' ? 'active' : ''}`}>
                 <div className='text-left text-white'>
                   <div className='flex'>
-                    <p className=" border border-black dark:border-white rounded-md text-center w-96"> • {translations[currentLanguage].firstAcademicTitle}</p>
+                    <p className=" border border-black dark:border-white rounded-md text-center light:text-black dark:text-white w-96"> • {translations[currentLanguage].firstAcademicTitle}</p>
                   </div>
                   <p className="mt-1 text-xs font-medium text-center">{translations[currentLanguage].firstAcademict}</p>
                   <p className="mt-1 text-xs font-medium text-center">2019 - 2023</p>
@@ -144,7 +144,7 @@ const Section = ({ currentLanguage }: AboutProps) => {
               <div onMouseEnter={() => handleSpanHover('fourthSpan')} onMouseLeave={() => handleSpanHover(null)} className={`zoom-effect flex items-start mt-3 text-base leading-relaxed text-black dark:text-black p-1.5 ${hoveredSpan === 'fourthSpan' ? 'active' : ''}`}>
                 <div className='text-left text-white'>
                   <div className='flex'>
-                    <p className=" border border-black dark:border-white rounded-md text-center w-96"> • {translations[currentLanguage].secondAcademicTitle}</p>
+                    <p className=" border border-black dark:border-white rounded-md text-center light:text-black dark:text-white w-96"> • {translations[currentLanguage].secondAcademicTitle}</p>
 
                   </div>
                   <p className="mt-1 text-xs font-medium text-center">{translations[currentLanguage].secondAcademic}</p>
