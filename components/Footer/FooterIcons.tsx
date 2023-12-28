c885896333959b56150595ea2ab51cdfd18d6f00
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-27 21:07:48 -0300

    Update FooterIcons.tsx

--- components/Footer/FooterIcons.tsx
+++ components/Footer/FooterIcons.tsx
@@ -69,7 +69,7 @@ const FooterIcons = ({currentLanguage }: FooterIconsProps) => {
     <div className="flex h-full">
       <div className={`${!copySuccess && shouldApplyWidthClass ? 'w-[15.8rem]' : ''}`}>
         {copySuccess && (
-          <p className={`${shouldApplyWidthClass ? 'flex-grow flex text-fuchsia-500 w-[17.5rem] h-full bg-white p-3 text-xs leading-normal transition duration-150 ease-in-out' : 'flex-grow text-fuchsia-500 w-[8rem] h-full bg-white p-3 text-xs leading-normal transition duration-150 ease-in-out'}`}>
+          <p className={`${shouldApplyWidthClass ? 'flex-grow flex text-fuchsia-500 w-[17.5rem] h-full p-3 text-xs leading-normal' : 'flex-grow text-fuchsia-500 w-[8rem] h-full p-3 text-xs leading-normal'}`}>
             <span className={`${shouldApplyWidthClass ? 'text-slate-600 text-xs' : 'hidden'}`}>{translations[currentLanguage].copyMessage}😎</span>
             <span className="ml-1 flex text-xs">{copiedValue} {translations[currentLanguage].copyValue}<RiFileCopyFill className={`${shouldApplyWidthClass ? 'ml-1 visible w-4 h-4 text-xs' : 'hidden'}`} /></span>
           </p>
@@ -77,7 +77,7 @@ const FooterIcons = ({currentLanguage }: FooterIconsProps) => {
       </div>
         
       <button
-        className={'flex hover:bg-fuchsia-600/50 light:text-black dark:text-white p-3 text-xs font-thin  uppercase leading-normal transition delay-100 duration-200 ease-in active:text-gray-400'}
+        className={'flex hover:bg-fuchsia-600 light:text-black dark:text-white p-3 text-xs font-thin  uppercase leading-normal transition delay-100 duration-200 ease-in active:text-gray-400'}
         onClick={() => handleCopyToClipboard("+55(12)997263748", "Phone")}
       >
         <TbPhoneCalling className="lg:w-[1.2rem] lg:h-[1.2rem] md:w-[1.2rem] md:h-[1.2rem] sm:w-[1.2rem] sm:h-[1.2rem] w-[1.2rem] h-[1.2rem] transition-colors duration-200" />
@@ -85,7 +85,7 @@ const FooterIcons = ({currentLanguage }: FooterIconsProps) => {
       </button>
         
       <button
-        className={'flex hover:bg-fuchsia-600/50 light:text-black dark:text-white p-3 text-xs font-thin uppercase leading-normal transition delay-100 duration-200 ease-in active:text-gray-400 '}
+        className={'flex hover:bg-fuchsia-600 light:text-black dark:text-white p-3 text-xs font-thin uppercase leading-normal transition delay-100 duration-200 ease-in active:text-gray-400 '}
         onClick={() => handleCopyToClipboard("diegoe.r.c07@gmail.com", "Mail")}
       >
         <TfiEmail className="lg:w-[1rem] lg:h-[1rem] md:w-[1rem] md:h-[1rem] sm:w-[1rem] sm:h-[1rem] w-[1rem] h-[1rem] transition-colors duration-200" />
