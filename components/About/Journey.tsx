898ab19a6703dca8ca56c6393f365847b5ada8ff
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-28 16:53:55 -0300

    Update Journey.tsx

--- components/About/Journey.tsx
+++ components/About/Journey.tsx
@@ -5,7 +5,6 @@ interface JourneyProps {
 }
 
 const Journey = ({ currentLanguage }: JourneyProps) => (
-  <div className="text-sm tracking-tight font-medium leading-relaxed text-white dark:text-black">
     <p className="mt-10">
       {translations[currentLanguage].journeyP1}
       <strong className="text-black dark:text-white uppercase">
@@ -13,21 +12,6 @@ const Journey = ({ currentLanguage }: JourneyProps) => (
       </strong>
       {translations[currentLanguage].journeyP3}
     </p>
-    <p className="mt-3">
-      {translations[currentLanguage].careerP1}
-      <strong className="text-black dark:text-white uppercase">
-        {translations[currentLanguage].careerP2}
-      </strong>
-      {translations[currentLanguage].careerP3}
-    </p>
-    <p className="mt-3">
-      {translations[currentLanguage].freeTimeP1}
-      <strong className="text-black dark:text-white uppercase">
-        {translations[currentLanguage].freeTimeP2}
-      </strong>
-      {translations[currentLanguage].freeTimeP3}
-    </p>
-  </div>
 );
 
 export default Journey;
