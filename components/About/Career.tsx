87c3dc38a92945b7bd56a77c1efa4d53c700ec9f
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-28 16:51:01 -0300

    Create Career.tsx

---
+++ components/About/Career.tsx
@@ -0,0 +1,17 @@
+import translations from './Translations';
+
+interface CareerProps {
+  currentLanguage: string;
+}
+
+const Career = ({ currentLanguage }: CareerProps) => (
+    <p className="mt-3">
+      {translations[currentLanguage].careerP1}
+      <strong className="text-black dark:text-white uppercase">
+        {translations[currentLanguage].careerP2}
+      </strong>
+      {translations[currentLanguage].careerP3}
+    </p>
+);
+
+export default Journey;
