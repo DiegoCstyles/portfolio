fc1bf3f2d46f279b45c1a95abebc7c62774e51a3
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-28 18:33:44 -0300

    Update Translations.ts

--- components/Hero/Translations.ts
+++ components/Hero/Translations.ts
@@ -5,11 +5,11 @@ interface HeroTranslations {
 
 const translations: { [language: string]: HeroTranslations } = {
   en: {
-    title: 'FullStack developer.',
+    title: 'Full-Stack developer.',
     description: 'Passionate about building responsive and dynamic web applications with React, Next.js, and Tailwind CSS.',
   },
   'pt-br': {
-    title: 'Desenvolvedor FullStack.',
+    title: 'Desenvolvedor Full-Stack.',
     description: 'Apaixonado por construir aplicações web responsivas e dinâmicas com React, Next.js e Tailwind CSS.',
   },
 };
