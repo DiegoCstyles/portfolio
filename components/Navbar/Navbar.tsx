3ea7851b6a150c5c13e83b85169af615ac9f12d7
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-29 20:23:16 -0300

    Update Navbar.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { NavbarTitle } from '@/components'
 import { NavbarPage } from '@/components'
 import { SetTheme } from '@/components'
