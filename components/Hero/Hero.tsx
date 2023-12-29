5824f4d8fde6f03f2bdd9724cebce9294c33b295
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-29 20:43:12 -0300

    Update Hero.tsx

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -12,42 +12,8 @@ interface HeroProps {
 const Hero = ({ currentLanguage }: HeroProps) => {
   const isMobile = useMediaQuery({ maxWidth: 1024 });
 
-  useEffect(() => {
-    const handleScroll = () => {
-      // Get the vertical scroll position
-      const scrollPosition = window.scrollY;
-      
-      // Calculate the amount of offset for the image (inverted direction)
-      const offsetSection = scrollPosition * 0.1;
-
-      // Apply the offset to the image using CSS variables
-      document.documentElement.style.setProperty('--section-offset', `${offsetSection}px`);
-    };
-
-    // Attach the event listener for scroll
-    window.addEventListener('scroll', handleScroll);
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
     <section className="relative hero min-h-screen flex-auto py-24 flex items-center justify-center light:bg-white dark:bg-black">
-      <style>{`
-        :root {
-          --section-offset: 0;
-        }
-
-        .ltBackground {
-          transform: translateY(var(--section-offset));
-        }
-
-        .dkBackground {
-          transform: translateY(var(--section-offset));
-        }
-      `}</style>
       
       <div className="mx-auto max-w-[80rem] text-center animate-fade-in">
         {/* Heading and Contact Icons */}
