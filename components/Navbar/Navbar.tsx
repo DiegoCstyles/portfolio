8702e04cc38637b643c2ecb016693edffd526da5
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-29 23:09:46 -0300

    Update Navbar.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -17,6 +17,11 @@ const Navbar = ({ currentLanguage, onLanguageChange }: NavbarProps) => {
   const [isNavbarVisible, setNavbarVisible] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
 
+  const handleMobileMenuToggle = () => {
+    // Toggle visibility of mobile menu
+    setNavbarVisible(!isNavbarVisible);
+  };
+
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
@@ -67,13 +72,27 @@ const Navbar = ({ currentLanguage, onLanguageChange }: NavbarProps) => {
             )}
 
             {/* Mobile menu */}
-            <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 dark:bg-white" aria-controls="navbar-default" aria-expanded="false" aria-label="Navbar">
-              <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="#"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+            <button
+              data-collapse-toggle="navbar-default"
+              type="button"
+              className="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 dark:bg-white"
+              aria-controls="navbar-default"
+              aria-expanded="false"
+              aria-label="Navbar"
+              onClick={handleMobileMenuToggle}
+            >
+              <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="#">
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
             </button>
           </div>
 
           {/* Pages */}
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div className={`w-full md:w-auto ${isSmallScreen && !isNavbarVisible ? 'hidden' : 'block'}`} id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:mt-0">
               <li
                 className={`cursor-pointer block text-sm p-3 dark:text-white transition delay-100 duration-200 ease-in zoom-effect hover:bg-fuchsia-600`}
