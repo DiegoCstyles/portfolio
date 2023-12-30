4f38cc6cd92a335e8acf6a2433bbf09f7bf879b0
Diego <124840180+DiegoCstyles@users.noreply.github.com>
2023-12-30 08:17:54 -0300

    Update Navbar.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -93,7 +93,7 @@ const Navbar = ({ currentLanguage, onLanguageChange }: NavbarProps) => {
             id="navbar-default"
           >
             {isSmallScreen ? (
-              <div className={isNavbarVisible ? 'bg-black/90' : ''}>
+              <div className={isNavbarVisible ? 'bg-white/90 dark:bg-black/90 z-40' : ''}>
                 <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:mt-0">
                   <li
                     className={`cursor-pointer block text-sm p-3 dark:text-white transition delay-100 duration-200 ease-in zoom-effect hover:bg-fuchsia-600`}
